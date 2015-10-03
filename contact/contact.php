9929ad0976f01a06f4116da1bfaeebfe12a96dba
expressholidays <expresholidays@gmail.com>
2015-10-03 09:00:20 +0600

    contact form update

--- contact/contact.php
+++ contact/contact.php
@@ -1,35 +1,35 @@
 <!-- Contact form PHP script -->
 
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $message = $_POST['message'];
 
-    $to = "khojkori@gmail.com"; // Replace xxxx@xxxx.com with your email address (mandatory!) 
-    $subject = "Hello"; // Choose a custom subject (not mandatory)
+    // $to = "expresholidays@gmail.com"; // Replace xxxx@xxxx.com with your email address (mandatory!) 
+    // $subject = "Hello"; // Choose a custom subject (not mandatory)
 
-    $body = "You have received a message from " . $name . " (" . $email . "):\n\n" . $message;
+    // $body = "You have received a message from " . $name . " (" . $email . "):\n\n" . $message;
 
-    $from = "From: Beetle Template"; // Replace "Beetle Template" with your site name (not mandatory)   
-    $headers = "From:" . $from . "\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "X-Mailer: PHP/" . phpversion();    
+    // $from = "From: Beetle Template"; // Replace "Beetle Template" with your site name (not mandatory)   
+    // $headers = "From:" . $from . "\r\n";
+    // $headers .= "Reply-To: " . $email . "\r\n";
+    // $headers .= "X-Mailer: PHP/" . phpversion();    
                 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        if ($name != '' && $email != '' && $message != '') {
+    // if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //     if ($name != '' && $email != '' && $message != '') {
                 
-            if (mail ($to, $subject, $body, $headers)) { 
-                echo '<p style="color:#66A325;">Thanks! Your message has been sent.</p>';
-            } else { 
-                echo '<p style="color:#F84B3C;">Something went wrong, go back and try again!</p>'; 
-            } 
+    //         if (mail ($to, $subject, $body, $headers)) { 
+    //             echo '<p style="color:#66A325;">Thanks! Your message has been sent.</p>';
+    //         } else { 
+    //             echo '<p style="color:#F84B3C;">Something went wrong, go back and try again!</p>'; 
+    //         } 
 
-        } else {
-            echo '<p style="color:#F84B3C;">You need to fill in all required fields!</p>';
-        }
-    } else {
-        echo '<p style="color:#F84B3C;">Invalid Email, please provide an correct email.</p>';
-    }  
+    //     } else {
+    //         echo '<p style="color:#F84B3C;">You need to fill in all required fields!</p>';
+    //     }
+    // } else {
+    //     echo '<p style="color:#F84B3C;">Invalid Email, please provide an correct email.</p>';
+    // }  
 
     // Use the script below if you want to use HTML emails instead
     
@@ -37,7 +37,7 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $to = 'khojkori@gmail.com'; // Replace xxxx@xxxx.com with your email address (mandatory!) 
+    $to = 'expresholidays@gmail.com'; // Replace xxxx@xxxx.com with your email address (mandatory!) 
     $subject = 'Hello'; // Choose a custom subject (not mandatory)
 
     $body = '<html><body>';
